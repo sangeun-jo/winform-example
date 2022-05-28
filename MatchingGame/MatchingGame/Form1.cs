b22c4fbc8f6b17e7b41124cba7e1711223ab9553
sangeun-jo <sangeun.jo@outlook.kr>
2022-05-28 16:25:15 -0700

    add MatchingGame example

---
+++ MatchingGame/MatchingGame/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MatchingGame
+{
+    public partial class Form1 : Form
+    {
+        Label firstClicked = null;
+        Label secondClicked = null; 
+
+        Random random = new Random();
+        
+        List<string> icons = new List<string>() {
+            "!", "!", "N", "N", ",", ",", "k", "k", "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled == true)
+                return;
+
+            Label clickedLabel = sender as Label;
+
+            if (clickedLabel != null) {
+                if (clickedLabel.ForeColor == Color.Black) 
+                    return;
+
+                if (firstClicked == null) {
+                    firstClicked = clickedLabel; 
+                    clickedLabel.ForeColor = Color.Black;
+                    return;
+                }
+
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.Black;
+
+                CheckForWinner();
+
+                if (firstClicked.Text == secondClicked.Text) {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+            }
+
+            timer1.Start(); 
+        }
+
+        private void AssignIconsToSquares() {
+            foreach (Control control in tableLayoutPanel1.Controls) {
+                Label iconLable = control as Label;
+                if (iconLable != null) {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLable.Text = icons[randomNumber];
+                    iconLable.ForeColor = iconLable.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+                
+            }
+        }
+
+        private void CheckForWinner() {
+            foreach (Control control in tableLayoutPanel1.Controls) {
+                Label iconLabel = control as Label;
+
+                if (iconLabel != null) {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                        return; 
+                }
+            }
+
+            MessageBox.Show("You matched all the icons!", "Congratuations");
+            Close();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            firstClicked = null;
+            secondClicked = null;
+        }
+    }
+}
